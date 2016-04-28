aeb346061c2999e1a58ffe6a5964c462bd6593d6
gitizenme <joe@izen.me>
2016-04-28 07:05:10 -0700

    Working on the Prize DB

---
+++ robots/raffl/db/prize.go
@@ -0,0 +1,159 @@
+package prize
+
+
+import (
+	"fmt"
+	"log"
+	"time"
+	"path"
+	"bytes"
+	"runtime"
+	"encoding/json"
+	"encoding/gob"
+
+	"github.com/boltdb/bolt"
+)
+
+var db *bolt.DB
+var open bool
+const PrizeBucketName = "prizes"prizeBucketName
+
+type Prize struct {
+	ID string
+	Title   string
+	Description string
+	LicenseKey string
+	Claimed bool
+	Username string
+}
+
+func Open() error {
+	var err error
+	_, filename, _, _ := runtime.Caller(0)  // get full path of this file
+	dbfile := path.Join(path.Dir(filename), "data.db")
+	config := &bolt.Options{Timeout: 1 * time.Second}
+	db, err = bolt.Open(dbfile, 0600, config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	open = true
+	return nil
+}
+
+func Close() {
+	open = false
+	db.Close()
+}
+
+func (p *Prize) Save() error {
+	if !open {
+		return fmt.Errorf("db must be opened before saving!")
+	}
+	err := db.Update(func(tx *bolt.Tx) error {
+		people, err := tx.CreateBucketIfNotExists([]byte(PrizeBucketName))
+		if err != nil {
+			return fmt.Errorf("create bucket: %s", err)
+		}
+		enc, err := p.encode()
+		if err != nil {
+			return fmt.Errorf("could not encode Person %s: %s", p.ID, err)
+		}
+		err = people.Put([]byte(p.ID), enc)
+		return err
+	})
+	return err
+}
+
+func (p *Prize) gobEncode() ([]byte, error) {
+	buf := new(bytes.Buffer)
+	enc := gob.NewEncoder(buf)
+	err := enc.Encode(p)
+	if err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+func gobDecode(data []byte) (*Prize, error) {
+	var p *Prize
+	buf := bytes.NewBuffer(data)
+	dec := gob.NewDecoder(buf)
+	err := dec.Decode(&p)
+	if err != nil {
+		return nil, err
+	}
+	return p, nil
+}
+
+func (p *Prize) encode() ([]byte, error) {
+	enc, err := json.Marshal(p)
+	if err != nil {
+		return nil, err
+	}
+	return enc, nil
+}
+
+func decode(data []byte) (*Prize, error) {
+	var p *Prize
+	err := json.Unmarshal(data, &p)
+	if err != nil {
+		return nil, err
+	}
+	return p, nil
+}
+
+func GetPrize(id string) (*Prize, error) {
+	if !open {
+		return nil, fmt.Errorf("db must be opened before saving!")
+	}
+	var p *Prize
+	err := db.View(func(tx *bolt.Tx) error {
+		var err error
+		b := tx.Bucket([]byte(PrizeBucketName))
+		k := []byte(id)
+		p, err = decode(b.Get(k))
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		fmt.Printf("Could not get Prize ID %s", id)
+		return nil, err
+	}
+	return p, nil
+}
+
+func List(bucket string) {
+	db.View(func(tx *bolt.Tx) error {
+		c := tx.Bucket([]byte(bucket)).Cursor()
+		for k, v := c.First(); k != nil; k, v = c.Next() {
+			fmt.Printf("key=%s, value=%s\n", k, v)
+		}
+		return nil
+	})
+}
+
+func ListPrefix(bucket, prefix string) {
+	db.View(func(tx *bolt.Tx) error {
+		c := tx.Bucket([]byte(bucket)).Cursor()
+		p := []byte(prefix)
+		for k, v := c.Seek(p); bytes.HasPrefix(k, p); k, v = c.Next() {
+			fmt.Printf("key=%s, value=%s\n", k, v)
+		}
+		return nil
+	})
+}
+
+func ListRange(bucket, start, stop string) {
+	db.View(func(tx *bolt.Tx) error {
+		c := tx.Bucket([]byte(bucket)).Cursor()
+		min := []byte(start)
+		max := []byte(stop)
+		for k, v := c.Seek(min); k != nil && bytes.Compare(k, max) <= 0;
+		k, v = c.Next() {
+			fmt.Printf("%s: %s\n", k, v)
+		}
+		return nil
+	})
+}
--- robots/raffl/raffl.go
+++ robots/raffl/raffl.go
@@ -4,14 +4,57 @@ import (
 	"fmt"
 
 	"github.com/trinchan/slackbot/robots"
+	"github.com/boltdb/bolt"
 	"math/rand"
+	"log"
+	"time"
+	"encoding/json"
+	"github.com/trinchan/slackbot/robots/raffl/db"
+	"boldtb/person"
 )
+
+type Prize struct {
+	Created time.Time
+	Title   string
+	Description string
+	LicenseKey string
+	Claimed bool
+	Username string
+}
 
 type bot struct{}
 
 func init() {
 	p := &bot{}
 	robots.RegisterRobot("raffl", p)
+
+	prize.Open()
+	defer prize.Close()
+
+	// A Person struct consists of ID, Name, Age, Job.
+	prizes := []*prize.Prize{
+		{"100", "JetBrains product license", "1 year subscription to any product", "11112222", false, ""},
+	}
+
+	// Persist people in the database.
+	for _, p := range prizes {
+		p.Save()
+	}
+
+	// Get a prize from the database by their ID.
+	for _, id := range []string{"100", "101"} {
+		p, err := person.GetPerson(id)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(p)
+	}
+
+
+	person.List(prize.PrizeBucketName)                     // each key/val in people bucket
+	person.ListPrefix(prize.PrizeBucketName, "20")         // ... with key prefix `20`
+	person.ListRange(prize.PrizeBucketName, "101", "103")  // ... within range `101` to `103`
+
 }
 
 func (pb bot) Run(p *robots.Payload) (slashCommandImmediateReturn string) {
@@ -21,18 +64,36 @@ func (pb bot) Run(p *robots.Payload) (slashCommandImmediateReturn string) {
 
 func (pb bot) DeferredAction(p *robots.Payload) {
 
-	reasons := make([]string, 0)
-	reasons = append(reasons,
-		"Raffle Item 1",
-		"Raffle Item 2",
-		"Raffle Item 3",
-		"Raffle Item 4")
+
+	db, err := bolt.Open("my.db", 0600, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	rafflePrizes := make([]Prize, 0)
+
+	db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("prizes"))
+		b.ForEach(func(k, v []byte) error {
+			fmt.Printf("key=%s, value=%s\n", k, v)
+				var p *Prize;
+			err := json.Unmarshal(v, &p)
+			if err != nil {
+				return err
+			}
+			rafflePrizes = append(rafflePrizes, p)
+			return nil
+		})
+		return nil
+	})
+
 
 	pick := rand.Intn(8)
 
 	message := ""
-	if pick > 0 && pick < len(reasons) {
-		message = fmt.Sprintf("Your a winner! Here is your prize: %s", reasons[pick])
+	if pick > 0 && pick < len(rafflePrizes) {
+		message = fmt.Sprintf("Your a winner! Here is your prize: %s", rafflePrizes[pick])
 	} else {
 		message = "Sorry, better luck next time!"
 	}
